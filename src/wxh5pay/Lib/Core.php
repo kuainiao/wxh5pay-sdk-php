8f731e35890fb631c57ed53d29da256694b5bc59
adcbguo <adcbguo@qq.com>
2017-10-12 11:23:57 +0800

    修改证书

--- src/wxh5pay/Lib/Core.php
+++ src/wxh5pay/Lib/Core.php
@@ -88,12 +88,11 @@ class Core
         //要求结果为字符串且输出到屏幕上
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         if ($useCert == true) {
-            //设置证书
             //使用证书：cert 与 key 分别属于两个.pem文件
             curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM');
-            //curl_setopt($ch,CURLOPT_SSLCERT, WxPayConfig::SSLCERT_PATH);
+            curl_setopt($ch,CURLOPT_SSLCERT, '证书地址');
             curl_setopt($ch, CURLOPT_SSLKEYTYPE, 'PEM');
-            //curl_setopt($ch,CURLOPT_SSLKEY, WxPayConfig::SSLKEY_PATH);
+            curl_setopt($ch,CURLOPT_SSLKEY, '证书地址');
         }
         //post提交方式
         curl_setopt($ch, CURLOPT_POST, TRUE);
